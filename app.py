eab13f105c19aa6260ae0585ce2939d16083a2d4
ssikorski178 <60953154+ssikorski178@users.noreply.github.com>
2020-04-05 20:04:33 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 ## app.py
 
 from flask import Flask
+import hashlib
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,12 +11,24 @@ def howdy():
     return "<h1>Hello World</h1>"
 
 @app.route("/md5/<string>")
-def md5():
-    return
-
-@app.route("/factorial/<int>")
-def fact():
-    return
+def md5(string):
+	hash_object = hashlib.md5(string.encode())
+	md5_hash = hash_object.hexdigest()
+	return(md5_hash)
+    
+@app.route("/factorial/<num>")
+def fact(num):
+	intnum = int(num)
+	factorial = 1
+	if intnum == 0:
+		return 'Factorial of 0 is 1'
+	if intnum < 0:
+		return 'Error, please enter a positive integer'
+	else:
+		for i in range(1,intnum + 1):
+			factorial = factorial*i
+		return(str(factorial))
+    
 
 @app.route("/fibonacci/<int>")
 def fib():
