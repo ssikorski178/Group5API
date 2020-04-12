753f32ca3a0fce218af51a1b29b7f5c61f6a06ec
ssikorski178 <60953154+ssikorski178@users.noreply.github.com>
2020-04-12 11:50:46 -0500

    Add files via upload
    Fixed first two functions not returning values as json data.

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ def howdy():
 def md5(string):
 	hash_object = hashlib.md5(string.encode())
 	md5_hash = hash_object.hexdigest()
-	return(md5_hash)
+	return jsonify(md5_hash)
     
 @app.route("/factorial/<num>")
 def fact(num):
 	intnum = int(num)
 	factorial = 1
 	if intnum == 0:
-		return 'Factorial of 0 is 1'
+		return jsonify('Factorial of 0 is 1')
 	if intnum < 0:
-		return 'Error, please enter a positive integer'
+		return jsonify('Error, please enter a positive integer')
 	else:
 		for i in range(1,intnum + 1):
 			factorial = factorial*i
-		return(str(factorial))
+		return jsonify(str(factorial))
     
 
 @app.route('/fibonacci/<int(signed=True):x>')
